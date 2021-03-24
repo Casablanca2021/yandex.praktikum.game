24e0a60b1abbbf495090a3c22122eb324249bed9
Artem Nikulin <temych.nikulin@yandex.ru>
2021-03-24 10:57:43 +0300

    Add registration page

--- src/common/dictionary.ts
+++ src/common/dictionary.ts
@@ -1,15 +1,15 @@
 const dictionary = {
-  first_name: "Имя",
-  second_name: "Фамилия",
-  login: "Логин",
-  email: "Почта",
-  phone: "Телефон",
-  password: "Пароль",
-  password_confirm: "Пароль (еще раз)",
-  signupTitle: "Регистрация",
-  signinTitle: "Вход",
-  signupButton: "Зарегистрироваться",
-  signinButton: "Войти",
+  first_name: 'Имя',
+  second_name: 'Фамилия',
+  login: 'Логин',
+  email: 'Почта',
+  phone: 'Телефон',
+  password: 'Пароль',
+  password_confirm: 'Пароль (еще раз)',
+  signupTitle: 'Регистрация',
+  signinTitle: 'Вход',
+  signupButton: 'Зарегистрироваться',
+  signinButton: 'Войти',
 };
 
 export const t = (key: keyof typeof dictionary): string => dictionary[key];
