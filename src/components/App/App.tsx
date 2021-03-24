7973397448c0c151490a6c6f842b7fe7363b1f11
Artem Nikulin <temych.nikulin@yandex.ru>
2021-03-24 10:57:43 +0300

    Add registration page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,20 @@
-import React, {FC} from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {ROUTES} from 'common/consts';
-import './app.css';
+import React, { FC } from "react";
+import { Switch, Route } from "react-router-dom";
+import { ROUTES } from "common/consts";
+import "./App.css";
 
-export const App: FC = () => {
-	return (
-		<Switch>
-			<Route path={ROUTES.SIGNIN}>
-				<h1>Signin</h1>
-			</Route>
-			<Route path={ROUTES.SIGNUP}>
-				<h1>Signup</h1>
-			</Route>
-			<Route path={ROUTES.ROOT}>
-				<div className="appcomponent">
-					<h1>App component</h1>
-				</div>
-			</Route>
-		</Switch>
-	);
-};
+export const App: FC = () => (
+  <Switch>
+    <Route path={ROUTES.SIGNIN}>
+      <h1>Signin</h1>
+    </Route>
+    <Route path={ROUTES.SIGNUP}>
+      <h1>Signup</h1>
+    </Route>
+    <Route path={ROUTES.ROOT}>
+      <div className="appcomponent">
+        <h1>App component</h1>
+      </div>
+    </Route>
+  </Switch>
+);
