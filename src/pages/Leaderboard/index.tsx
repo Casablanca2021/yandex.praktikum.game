7e51c01c6b67e433195dc743864d152fe8c023e1
Kerimov Alikhan <kerimov95@mail.ru>
2021-03-28 23:10:22 +0300

    Add Leaderboard Page

---
+++ src/pages/Leaderboard/index.tsx
@@ -0,0 +1,7 @@
+import Leaderboard from 'pages/Leaderboard/Leaderboard';
+import { LeaderboardPage, Leaders } from 'pages/Leaderboard/types';
+import LeaderboardServices from 'pages/Leaderboard/services';
+
+export {
+  Leaderboard, LeaderboardPage, Leaders, LeaderboardServices,
+};
