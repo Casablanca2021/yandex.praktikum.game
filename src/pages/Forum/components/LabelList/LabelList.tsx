5a4206ada880031e3161ff24234fe9991c533fb4
simeon <shayhutdinov.s@perfluence.io>
2021-04-02 19:33:18 +0300

    cb-8 (фиксы после ревью 2)

--- src/pages/Forum/components/LabelList/LabelList.tsx
+++ src/pages/Forum/components/LabelList/LabelList.tsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 const LabelList: FC<OwnProps> = (props) => {
   return (
     <div className="label-list">
-      {/* eslint-disable-next-line react/destructuring-assignment */}
       {props.items.map((item, key) => <Link to={item.path} key={key} className={`label-list__item ${item.active ? 'label-list__item_active' : ''}`}>{item.name}</Link>)}
     </div>
   );
