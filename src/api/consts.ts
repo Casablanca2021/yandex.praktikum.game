a5c24bcf529a01521fec8f44f907a770ce18a349
Artem Nikulin <temych.nikulin@yandex.ru>
2021-05-26 16:23:17 +0300

    Add yandex oauth

---
+++ src/api/consts.ts
@@ -0,0 +1,20 @@
+export const baseHost = 'https://ya-praktikum.tech';
+export const baseUrl = `${baseHost}/api/v2`;
+export const baseUrlResources = `${baseHost}/api/v2/resources`;
+
+export const headersJSON = {
+  'Content-Type': 'application/json',
+};
+
+export const redirectUri = 'https://casablanca-racing.herokuapp.com';
+export const yandexOauthUrl = 'https://oauth.yandex.ru/authorize?response_type=code';
+
+export const ApiPath = {
+  SIGN_UP: `${baseUrl}/auth/signup`,
+  SIGN_IN: `${baseUrl}/auth/signin`,
+  LOG_OUT: `${baseUrl}/auth/logout`,
+  USER: `${baseUrl}/auth/user`,
+  PROFILE: `${baseUrl}/user/profile`,
+  AVATAR: `${baseUrl}/user/profile/avatar`,
+  YANDEX_OAUTH_ID: `${baseUrl}/oauth/yandex/service-id`,
+};
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -3,3 +3,5 @@ import { AppState } from './types';
 export const getAuthSelector = (state: AppState) => state.auth;
 
 export const getUserSelector = (state: AppState) => state.user;
+
+export const getSSRSelector = (state: AppState) => state.ssr;
