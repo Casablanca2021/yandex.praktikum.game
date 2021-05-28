7fe2861de50deaf09061e728c45384d8effeef38
Kerimov Alikhan <kerimov95@mail.ru>
2021-05-29 00:39:18 +0300

    Add modgoDB

---
+++ backend/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  externals: [nodeExternals()],
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
